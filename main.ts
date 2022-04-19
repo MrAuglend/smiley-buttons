b172f2b7ee93f6b008fc3261992b0ce4a648cc5a
MrAuglend <103996341+MrAuglend@users.noreply.github.com>
2022-04-19 11:24:22 +0200

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.showString("Smiley")
